3dc2dfb1b3055f02535de203f4018b9c7d00f132
NovaFox161 <novamaday@gmail.com>
2018-04-19 07:55:38 -0500

    Info about new plugin. still need to add a few quick things.

--- src/main/java/com/novamaday/website/Main.java
+++ src/main/java/com/novamaday/website/Main.java
@@ -3,6 +3,7 @@ package com.novamaday.website;
 import com.novamaday.website.account.AccountHandler;
 import com.novamaday.website.database.DatabaseManager;
 import com.novamaday.website.objects.SiteSettings;
+import com.novamaday.website.utils.Logger;
 import com.novamaday.website.utils.SparkUtils;
 
 import java.io.File;
@@ -16,6 +17,8 @@ public class Main {
         Properties p = new Properties();
         p.load(new FileReader(new File("settings.properties")));
         SiteSettings.init(p);
+
+        Logger.getLogger().init();
 
         //Init database
         DatabaseManager.getManager().connectToMySQL();
