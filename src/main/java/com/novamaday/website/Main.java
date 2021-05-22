c3202d026461f6b5833c18bc1f01d8dbab7690ba
NovaFox161 <novamaday@gmail.com>
2021-05-22 14:15:41 -0500

    Clean up a ton of shit. This whole project needs to be rewritten
    Just cleaning up and removing stuff so I can stick this in k8s to try and get shit working and shutdown a VPS

--- src/main/java/com/novamaday/website/Main.java
+++ src/main/java/com/novamaday/website/Main.java
@@ -1,10 +1,6 @@
 package com.novamaday.website;
 
-import com.novamaday.website.account.AccountHandler;
-import com.novamaday.website.database.DatabaseManager;
 import com.novamaday.website.objects.SiteSettings;
-import com.novamaday.website.utils.EmailHandler;
-import com.novamaday.website.utils.Logger;
 import com.novamaday.website.utils.SparkUtils;
 
 import java.io.File;
@@ -16,20 +12,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //Load settings
         Properties p = new Properties();
-        p.load(new FileReader(new File("settings.properties")));
+        p.load(new FileReader("settings.properties"));
         SiteSettings.init(p);
 
-        Logger.getLogger().init();
-
-        //Init database
-        DatabaseManager.getManager().connectToMySQL();
-        DatabaseManager.getManager().createTables();
-
         //Init spark
-        AccountHandler.getHandler().init();
         SparkUtils.initSpark();
-
-        //Init the rest of our services
-        EmailHandler.getHandler().init();
     }
 }
--- src/main/java/com/novamaday/website/utils/SparkUtils.java
+++ src/main/java/com/novamaday/website/utils/SparkUtils.java
@@ -1,19 +1,24 @@
 package com.novamaday.website.utils;
 
-import com.novamaday.website.account.AccountHandler;
-import com.novamaday.website.api.v1.endpoints.AccountEndpoint;
-import com.novamaday.website.api.v1.endpoints.PluginUpdateEndpoint;
-import com.novamaday.website.database.DatabaseManager;
 import com.novamaday.website.objects.SiteSettings;
-import com.novamaday.website.objects.UserAPIAccount;
 import spark.ModelAndView;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
 public class SparkUtils {
-    @SuppressWarnings("ThrowableNotThrown")
+    private static Map<String, Object> map() {
+        Map m = new HashMap();
+        m.put("loggedIn", false);
+        m.put("year", LocalDate.now().getYear());
+        return m;
+    }
+
     public static void initSpark() {
-        port(Integer.valueOf(SiteSettings.PORT.get()));
+        port(Integer.parseInt(SiteSettings.PORT.get()));
 
         staticFileLocation("/web/public"); // Main site location
 
@@ -22,93 +27,22 @@ public class SparkUtils {
             return response.body();
         }));
 
-        //Register the API Endpoints
-        before("/api/*", (request, response) -> {
-            if (!request.requestMethod().equalsIgnoreCase("POST")) {
-                Logger.getLogger().api("Denied '" + request.requestMethod() + "' access", request.ip());
-                halt(405, "Method not allowed");
-            }
-            //Check authorization
-            if (AccountHandler.getHandler().hasAccount(request.session().id())) {
-                //User is logged in from website, no API key needed
-                Logger.getLogger().api("API Call from website", request.ip());
-            } else {
-                //Requires "Authorization" Header
-                if (request.headers().contains("Authorization")) {
-                    String key = request.headers("Authorization");
-                    //This if safe to do since we are using NoCaptcha ReCaptcha on these pages which verifies domain source.
-                    if (key.equals("REGISTER_ACCOUNT") && request.pathInfo().equals("/api/v1/account/register")) {
-                        Logger.getLogger().api("User registering account.", request.ip());
-                    } else if (key.equals("LOGIN_ACCOUNT") && request.pathInfo().equals("/api/v1/account/login")) {
-                        Logger.getLogger().api("user logging into account.", request.ip());
-                    } else if (key.equals("CHECK_PLUGIN") && request.pathInfo().equals("/api/v1/plugin/check")) {
-                        Logger.getLogger().api("MC Server checking plugin version.", request.ip());
-                    } else {
-                        UserAPIAccount acc = DatabaseManager.getManager().getAPIAccount(key);
-                        if (acc != null) {
-                            if (acc.isBlocked()) {
-                                Logger.getLogger().api("Attempted to use blocked API Key: " + acc.getAPIKey(), request.ip());
-                                halt(401, "Unauthorized");
-                            } else {
-                                //Everything checks out!
-                                acc.setUses(acc.getUses() + 1);
-                                DatabaseManager.getManager().updateAPIAccount(acc);
-                            }
-                        } else {
-                            Logger.getLogger().api("Attempted to use invalid API Key: " + key, request.ip());
-                            halt(401, "Unauthorized");
-                        }
-                    }
-                } else {
-                    Logger.getLogger().api("Attempted to use API without authorization header", request.ip());
-                    halt(400, "Bad Request");
-                }
-            }
-            //Only accept json because its easier to parse and handle.
-            if (!request.contentType().equalsIgnoreCase("application/json")) {
-                halt(400, "Bad Request");
-            }
-        });
-
-        //API endpoints
-        path("/api/v1", () -> {
-            before("/*", (q, a) -> System.out.println("Received API call from: " + q.ip() + "; Host:" + q.host()));
-            path("/account", () -> {
-                post("/register", AccountEndpoint::register);
-                post("/login", AccountEndpoint::login);
-            });
-            path("/plugin", () -> {
-                post("/add", PluginUpdateEndpoint::add);
-                post("/check", PluginUpdateEndpoint::check);
-                //TODO: Add update/download endpoint
-            });
-        });
-
         //Templates and pages...
-        get("/", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/index"), new ThymeleafTemplateEngine());
-        get("/home", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/index"), new ThymeleafTemplateEngine());
-        get("/about", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/about"), new ThymeleafTemplateEngine());
-        get("/contact", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/contact"), new ThymeleafTemplateEngine());
-
-        //Account pages
-        get("/account", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/account/account"), new ThymeleafTemplateEngine());
-        get("/account/register", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/account/register"), new ThymeleafTemplateEngine());
-        get("/account/login", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/account/login"), new ThymeleafTemplateEngine());
-        get("/account/logout", AccountEndpoint::logout);
+        get("/", (rq, rs) -> new ModelAndView(map(), "pages/index"), new ThymeleafTemplateEngine());
+        get("/home", (rq, rs) -> new ModelAndView(map(), "pages/index"), new ThymeleafTemplateEngine());
+        get("/about", (rq, rs) -> new ModelAndView(map(), "pages/about"), new ThymeleafTemplateEngine());
+        get("/contact", (rq, rs) -> new ModelAndView(map(), "pages/contact"), new ThymeleafTemplateEngine());
 
         //Art pages
-        get("/art/ap-studio", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/art/ap-studio"), new ThymeleafTemplateEngine());
-        get("/portfolio", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/art/portfolio"), new ThymeleafTemplateEngine());
+        get("/art/ap-studio", (rq, rs) -> new ModelAndView(map(), "pages/art/ap-studio"), new ThymeleafTemplateEngine());
+        get("/portfolio", (rq, rs) -> new ModelAndView(map(), "pages/art/portfolio"), new ThymeleafTemplateEngine());
 
         //Plugin pages
-        get("/plugins/perworldchatplus", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/plugins/perworldchatplus"), new ThymeleafTemplateEngine());
-        get("plugins/insane-warps", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/plugins/insanewarps"), new ThymeleafTemplateEngine());
-        get("plugins/novalib", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/plugins/novalib"), new ThymeleafTemplateEngine());
+        get("/plugins/perworldchatplus", (rq, rs) -> new ModelAndView(map(), "pages/plugins/perworldchatplus"), new ThymeleafTemplateEngine());
+        get("plugins/insane-warps", (rq, rs) -> new ModelAndView(map(), "pages/plugins/insanewarps"), new ThymeleafTemplateEngine());
+        get("plugins/novalib", (rq, rs) -> new ModelAndView(map(), "pages/plugins/novalib"), new ThymeleafTemplateEngine());
 
         //Policy pages
-        get("/policy/privacy", (rq, rs) -> new ModelAndView(AccountHandler.getHandler().getAccount(rq.session().id()), "pages/policy/privacy"), new ThymeleafTemplateEngine());
-
-        //Callback URLs for various stuffs
-        get("/confirm/email", AccountEndpoint::confirmEmail);
+        get("/policy/privacy", (rq, rs) -> new ModelAndView(map(), "pages/policy/privacy"), new ThymeleafTemplateEngine());
     }
 }
